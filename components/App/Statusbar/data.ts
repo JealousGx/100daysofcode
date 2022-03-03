37e902a95331282507b676ba6c3640bff9f200d8
JealousGx <khiljimateenn@gmail.com>
2022-03-03 16:18:28 +0500

    Status bar

--- components/App/Statusbar/data.ts
+++ components/App/Statusbar/data.ts
@@ -2,11 +2,23 @@ interface dataType {
   users: {
     id: number
     name: string
-    rank?: string | "Member"
-    img?: StaticImageData | null
+    rank: string
+    status: string
+    color: string
+    bgColor: string
+    img?: StaticImageData
   }[]
   ranks: string[]
 }
+
+const colors = [
+  "#5d8aa8",
+  "#e52b50",
+  "#9966cc",
+  "#915c83",
+  "#8db600",
+  "#7fffd4",
+]
 
 export const data: dataType = {
   users: [
@@ -14,50 +26,81 @@ export const data: dataType = {
       id: 1,
       name: "Jealous",
       rank: "Owner",
+      status: "online",
+      color: "text-red",
+      bgColor: "bg-[#5d8aa8]",
     },
     {
       id: 2,
       name: "Envy",
       rank: "Owner",
+      status: "offline",
+      color: "text-red",
+      bgColor: "bg-[#9966cc]",
     },
     {
       id: 3,
       name: "Spectre",
       rank: "Moderator",
+      status: "online",
+      color: "text-orange-500",
+      bgColor: "bg-[#e52b50]",
     },
     {
       id: 4,
       name: "Deo",
       rank: "Member",
+      status: "online",
+      color: "text-sky-600",
+      bgColor: "bg-[#7fffd4]",
     },
     {
       id: 5,
       name: "John",
       rank: "Member",
+      status: "online",
+      color: "text-sky-600",
+      bgColor: "bg-[#9966cc]",
     },
     {
       id: 6,
       name: "Smith",
       rank: "Hero",
+      status: "online",
+      color: "text-yellow",
+      bgColor: "bg-[#e52b50]",
     },
     {
       id: 7,
       name: "Neo",
       rank: "Hero",
+      status: "online",
+      color: "text-yellow",
+      bgColor: "bg-[#7fffd4]",
     },
     {
       id: 8,
       name: "Naruto",
       rank: "Member",
+      status: "online",
+      color: "text-sky-600",
+      bgColor: "bg-[#915c83]",
     },
     {
       id: 9,
       name: "Sasuke",
       rank: "Member",
+      status: "offline",
+      color: "text-sky-600",
+      bgColor: "bg-[#9966cc]",
     },
     {
       id: 10,
       name: "Kakashi",
+      rank: "Member",
+      status: "offline",
+      color: "text-sky-600",
+      bgColor: "bg-[#7fffd4]",
     },
   ],
   ranks: ["Owner", "Moderator", "Hero", "Member"],
--- components/App/Statusbar/index.tsx
+++ components/App/Statusbar/index.tsx
@@ -1,32 +1,53 @@
 import Image from "next/image"
 import React from "react"
 
-import Logo from "../../assets/server_logo.png"
+import Logo from "../../assets/Discord-Logo-White.png"
 import { data } from "./data"
 import { StatusContainer } from "./styles"
 
+interface usersType {
+  name: string
+  rank: string
+}
+
 const Index = () => {
-  const colors = [
-    "#5d8aa8",
-    "#e52b50",
-    "#9966cc",
-    "#915c83",
-    "#8db600",
-    "#7fffd4",
-  ]
-  const defaultImage = () => {
-    const randomColorIndex = Math.floor(Math.random() * 7)
-    ;<div className={`w-4 h-3 bg-[${colors[randomColorIndex]}]`}>
-      <Image src={Logo} alt="Profile" />
-      Lorem, ipsum.
-    </div>
-  }
   return (
     <StatusContainer>
       {data.ranks.map((rank, index) => {
+        let totalUsersInRank = 0
+        data.users.filter((user) => user.rank === rank && totalUsersInRank++)
         return (
-          <div className="py-4 text-gray-text font-bold uppercase" key={index}>
-            {rank}
+          <div className="py-4" key={index}>
+            <div className="text-gray-text text-sm font-bold uppercase pl-2">
+              {rank} - {totalUsersInRank}
+              <br />
+            </div>
+            {data.users.map((user) => {
+              return (
+                user.rank === rank && (
+                  <div
+                    key={user.id}
+                    className={`w-full py-2 pl-2 flex text-[1rem] font-semibold ${
+                      user.color
+                    } ${
+                      user.status === "offline" ? `opacity-50` : `opacity-100`
+                    } cursor-pointer hover:bg-[#34373C] rounded-[0.2rem]`}
+                  >
+                    <div
+                      className={`p-2 mr-2 flex items-center justify-center rounded-full ${user.bgColor}`}
+                    >
+                      <Image
+                        src={Logo}
+                        alt="Profile Logo"
+                        width={24}
+                        height={24}
+                      />
+                    </div>
+                    {user.name}
+                  </div>
+                )
+              )
+            })}
           </div>
         )
       })}
