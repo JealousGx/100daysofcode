553a6e32d40dd31a5cc2724d6d57e8fe20cfa8c5
JealousGx <khiljimateenn@gmail.com>
2022-07-27 09:36:03 +0500

    Backend changes

--- backend/Routes/User.js
+++ backend/Routes/User.js
@@ -8,7 +8,7 @@ router.get("/:id", async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.params.id });
     const { password, ...userInfo } = user._doc;
-    res.json(userInfo);
+    res.status(200).json(userInfo);
   } catch (err) {
     res.status(400).json({ msg: err.message });
   }
@@ -27,10 +27,10 @@ router.put("/:id", verify, async (req, res) => {
     return res.status(400).json({ msg: "User does not exist" }); // If the user does not exist, return an error.
   }
 
-  if (userWithID.email === user.email || req.user.isAdmin) {
+  if (req.params.id === req.user._id || req.user.isAdmin) {
     if (password) {
       const salt = await bcrypt.genSalt(10); // Generating a salt for the password (bcrypt).
-      password = await bcrypt.hash(password, salt); // Hashing the password (bcrypt).
+      req.body.password = await bcrypt.hash(password, salt); // Hashing the password (bcrypt).
     }
 
     try {
@@ -42,13 +42,13 @@ router.put("/:id", verify, async (req, res) => {
         { new: true }
       );
 
-      res.json(updatedUser);
+      res.status(200).json(updatedUser);
     } catch (err) {
-      res.status(400).json({ msg: err.message });
+      res.status(500).json({ msg: err.message });
     }
   } else {
     return res
-      .status(400)
+      .status(403)
       .json({ msg: "You are not authorized to update this user." }); // If the user tries to change the email, return an error.
   }
 });
@@ -61,18 +61,30 @@ router.delete("/:id", verify, async (req, res) => {
     return res.status(400).json({ msg: "User does not exist" }); // If the user does not exist, return an error.
   }
 
-  if (req.user.isAdmin || req.user.email === user.email) {
-    try {
-      const deletedUser = await User.findOneAndDelete({ _id });
-      res.json(deletedUser);
-    } catch (err) {
-      res.status(400).json({ msg: err.message });
+  try {
+    if (req.user.isAdmin) {
+      res.status(200).json("isAdmin");
+    } else if (req.user.id === req.params.id) {
+      res.status(200).json("isUser");
+    } else {
+      res.status(400).json("Null");
     }
-  } else {
-    return res
-      .status(400)
-      .json({ msg: "You are not authorized to update this user." }); // If the user tries to change the email, return an error.
+  } catch (err) {
+    res.status(403).json(err);
   }
+
+  // if (req.user._id === user._id || req.user.isAdmin) {
+  //   try {
+  //     const deletedUser = await User.findOneAndDelete({ _id });
+  //     res.status(200).json("deletedUser: ", deletedUser);
+  //   } catch (err) {
+  //     res.status(500).json({ msg: err.message });
+  //   }
+  // } else {
+  //   return res
+  //     .status(403)
+  //     .json({ msg: "You are not authorized to delete this user." }); // If the user tries to change the email, return an error.
+  // }
 });
 
 module.exports = router;
