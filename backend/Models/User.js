6cef9b09cc0832428c58628da703743ff2e7c113
JealousGx <khiljimateenn@gmail.com>
2022-07-28 15:03:06 +0500

    Backend complete: 100%

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const USER_BIO = "A Discord User";
+
 const userSchema = new mongoose.Schema(
   {
     id: { type: String, required: true, unique: true },
@@ -8,7 +10,7 @@ const userSchema = new mongoose.Schema(
     password: { type: String, required: true },
     profilePic: { type: String, default: "" },
     dateOfBirth: { type: Date, default: Date.now },
-    bio: { type: String, default: "" },
+    bio: { type: String, default: USER_BIO },
     isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }
