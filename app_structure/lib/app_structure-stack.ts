c6ec16162d171314cdc4f23df6678baedfa18d5b
Matt <59867217+JealousGx@users.noreply.github.com>
2022-08-26 17:13:58 +0000

    completes api infrastucture

--- app_structure/lib/app_structure-stack.ts
+++ app_structure/lib/app_structure-stack.ts
@@ -1,7 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as apiGateway from "aws-cdk-lib/aws-apigateway"
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as dotenv from "dotenv"
+
+dotenv.config()
 
 export class AppStructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,12 +14,30 @@ export class AppStructureStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    const lambdaAPI = new lambda.Function(this, "apiFunc", {
-      runtime: lambda.Runtime.PYTHON_3_9,
-      code: lambda.Code.fromAsset("../py-app/"),
-      handler: "api.handler"
+    const layer = new lambda.LayerVersion(this, 'BaseLayer', {
+      code: lambda.Code.fromAsset('./lambda_base_layer/layer.zip'),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_7],
     })
 
+    const lambdaAPI = new lambda.Function(this, "apiFunc", {
+      runtime: lambda.Runtime.PYTHON_3_7,
+      code: lambda.Code.fromAsset("../py-app/"),
+      handler: "api.handler",
+      layers: [layer],
+      environment: {
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? "",
+      }
+    })
+
+    const api = new apiGateway.RestApi(this, "api", {
+      restApiName: "Brand Assistant API",
+    })
+
+    const integration = new apiGateway.LambdaIntegration(lambdaAPI)
+    
+    api.root.addProxy({
+      defaultIntegration: integration,
+    })
     // example resource
     // const queue = new sqs.Queue(this, 'AppStructureQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
