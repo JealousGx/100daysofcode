1d874e0d40e6a6249194fd75ed5c7ea6028a33cd
Matt <59867217+JealousGx@users.noreply.github.com>
2022-08-26 18:12:47 +0000

    adds basic app site

---
+++ app-site/README.md
@@ -0,0 +1,2 @@
+# Brand Assistant App - Site
+This web app is a tool to help you generate keywords, description for your brand. It is a free service provided by [OpenAI](https://openai.com/). 
---
+++ app-site/pages/index.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Brand Assistant - JealousGx</title>
+        <meta
+          name="description"
+          content="A way to generate snippets and keywords for your brand. Enter your words and get started."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="fixed top-10 left-0 right-0 -z-10 lg:left-[12.5rem] lg:top-[15%]">
+        <div className="absolute left-8 top-16 -z-10 overflow-visible opacity-20">
+          <div className="circle-1 -z-10 h-[400px] w-[700px] rounded-full bg-red-700 mix-blend-multiply blur-[128px]" />
+        </div>
+        <div className="absolute left-10 top-32 -z-10 overflow-visible opacity-20">
+          <div className="circle-2 -z-10 h-[400px] w-[700px] rounded-full bg-green-700 mix-blend-multiply blur-[128px]" />
+        </div>
+        <div className="absolute left-12 top-48 -z-10 overflow-visible opacity-20">
+          <div className="circle-3 -z-10 h-[600px] w-[500px] rounded-full bg-blue-700 mix-blend-multiply blur-[128px]" />
+        </div>
+      </div>
+      <main className="container flex items-center justify-center">
+        <div className="bg-gray-600 rounded-lg max-w-sm h-96 w-full">
+          Brand Assistant
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
